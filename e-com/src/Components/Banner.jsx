a5bedcdaae2eaa6aa4cc4525427e33fa6daa45e7
ahad18 <abdulahad6361@gmail.com>
2024-10-16 00:59:11 +0500

    very new

--- e-com/src/Components/Banner.jsx
+++ e-com/src/Components/Banner.jsx
@@ -5,7 +5,7 @@ import mainbanner from '../assets/mainbanner.png'
 function Banner() {
   return (
     <div className="max-w-6xl mx-auto grid grid-cols-3 gap-4 py-11">
-            {/* Main Banner */}
+         
             <div className="col-span-2">
                 <img
                     src={mainbanner}
@@ -13,7 +13,7 @@ function Banner() {
                     className="w-full h-full object-cover rounded-lg"
                 />
             </div>
-            {/* Right side small banners */}
+           
             <div className="flex flex-col gap-4">
                 <img
                     src={banner}
